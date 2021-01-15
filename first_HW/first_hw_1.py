5219b2d40eb9a8ae8e985cf8fa1ce1172b92186e
alex123012 <makhonin.a.ru@gmail.com>
2021-01-16 01:49:03 +0300

    lol

--- first_HW/first_hw_1.py
+++ first_HW/first_hw_1.py
@@ -17,31 +17,31 @@ def quadratic_equation(a, b, c, rnd=4):
             return c**0.5
         else:
             x1 = (-c)**0.5
-            return  complex(f'{round(x1.imag, rnd)}j') + round(x1.real, rnd)
-        
+            x1 = complex(f'{round(x1.imag, rnd)}j') + round(x1.real, rnd)
+            return x1
 
-    dD = b**2 - 4*a*c
+    dD = b**2 - 4 * a * c
 
     if dD >= 0:
-        x1 = (-b + dD**0.5) / 2*a
+        x1 = (-b + dD**0.5) / (2 * a)
         if dD == 0:
             return x1
-        x2 = (-b - dD**0.5) / 2*a
+        x2 = (-b - dD**0.5) / (2 * a)
 
         return x1, x2
 
     else:
         sqrtD = dD**0.5
 
-        x1 = (-b + sqrtD**0.5) / 2*a
-        x2 = (-b - sqrtD**0.5) / 2*a
+        x1 = (-b + sqrtD**0.5) / (2 * a)
+        x2 = (-b - sqrtD**0.5) / (2 * a)
 
         x1 = complex(f'{round(x1.imag, rnd)}j') + round(x1.real, rnd)
         x2 = complex(f'{round(x2.imag, rnd)}j') + round(x2.real, rnd)
         # print(f'x1 = {x1}, x2 = {x2}')
 
         return x1, x2
-    
+
 
 def main():
     a = float(input('Enter coefficient A: '))
@@ -49,5 +49,6 @@ def main():
     c = float(input('Enter coefficient C: '))
     print('Quadratic roots are', quadratic_equation(a, b, c))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
--- first_HW/first_hw_2.py
+++ first_HW/first_hw_2.py
@@ -1,3 +1,3 @@
-a_list = list(map(float, input('Enter list of numbers divided with spaces ').split()))
-# a_list = [float(i) for i in input('Enter list of numbers divided with spaces ').split()]
-print(sum(a_list)/len(a_list))
+a_list = list(map(float,
+				  input('Enter list of numbers divided with spaces: ').split()))
+print(sum(a_list) / len(a_list))
