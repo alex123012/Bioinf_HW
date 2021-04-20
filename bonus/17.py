5e0310194b7c23006cc2aed40c0b5264567dc227
alex123012 <makhonin.a.ru@gmail.com>
2021-04-20 12:23:52 +0300

    add plot

--- bonus/17.py
+++ bonus/17.py
@@ -1,5 +1,8 @@
 from scipy.stats import binom
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 
 def bin_nll(data):
@@ -12,18 +15,38 @@ def bin_nll(data):
 
     tmp = y[tmp.index(max(tmp))]
     p = probs[tmp.index(max(tmp))]
-    return n, p
+    return n, p, x, y
 
 
 def main():
     x, y = 15, 0.5
     res_n, res_p = [], []
-    for _ in range(20):
-        data = binom.rvs(x, y, size=100)
-        n, p = bin_nll(data)
-        res_n.append(n), res_p.append(p)
-    print('mean n =', np.mean(res_n))
-    print('mean p =', np.mean(res_p))
+    # for _ in range(20):
+    data = binom.rvs(x, y, size=100)
+    n, p, x, y = bin_nll(data)
+    res_n.append(n), res_p.append(p)
+    print('n =', np.mean(res_n))
+    print('p =', np.mean(res_p))
+
+    probs = np.arange(0.01, 1, 0.01)
+    df = pd.DataFrame(y, columns=probs, index=x)
+
+    df = df.melt(ignore_index=False)
+
+    sns.set(style = "darkgrid")
+    sns.set_palette('Pastel1')
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection = '3d')
+    x = df.index
+    y = df.variable
+    z = df.value
+    ax.scatter(s=2, xs=x, ys=y, zs=z) #cmap=sns.color_palette('Pastel1', as_cmap=True))
+
+    ax.set_xlabel("n values")
+    ax.set_ylabel("p values")
+    ax.set_zlabel("NLL")
+
+    plt.show()
 
 
 if __name__ == '__main__':
